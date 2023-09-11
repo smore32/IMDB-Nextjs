b1b68d912d5990f003977700de9274d399671e5b
Shital <smore32@gmail.com>
2023-09-11 10:51:13 +0530

    Createing header section and test component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from "./components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/*Header*/}
+        {/*Header*/
+          <Header/>
+        }
 
         {/*Navbar*/}
 
